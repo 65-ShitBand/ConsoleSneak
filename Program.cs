c1f47eaab8eb7dba5c08dc5796a3681898f5ccbf
MihailEshman <esman6936@mail.com>
2023-03-09 03:37:58 +0300

    Добавил возможность задавать размер поля

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ using System.Text;
 namespace ConsoleSneak {
     class Program {
         public static void Main() {
+            Program p = new Program();
+            Console.Write("Задайте размер поля(min = 5): ");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 5) n = 5;
             Console.CursorVisible = false;
-            var a = new Game(10);
+            var a = new Game(n);
             while (true) a.StartGame();
         }
     }
--- Snake.cs
+++ Snake.cs
@@ -8,38 +8,38 @@ using System.Threading.Tasks;
 namespace ConsoleSneak {
     class Snake {
         public Queue<Point> body { get; private set; }
-
+        private int fieldSize;
         public Snake(int placeOfBirth) {
+            fieldSize = placeOfBirth;
             body = new Queue<Point>();
             body.Enqueue(new Point(placeOfBirth / 2, placeOfBirth / 2));
         }
-
         public Point Move(out Point furtherStep) {
             var pressedKey = Console.ReadKey();
             Point handler = body.Last();
-
+            
             switch (pressedKey.Key.ToString()) {
                 case "RightArrow":
                 case "D":
-                    if (handler.X + 1 == 9) handler.X = 1;
+                    if (handler.X + 1 == fieldSize - 1) handler.X = 1;
                     else handler.X += 1;
                     break;
 
                 case "LeftArrow":
                 case "A":
-                    if (handler.X - 1 == 0) handler.X = 8;
+                    if (handler.X - 1 == 0) handler.X = fieldSize - 2;
                     else handler.X -= 1;
                     break;
 
                 case "UpArrow":
                 case "W":
-                    if (handler.Y - 1 == 0) handler.Y = 8;
+                    if (handler.Y - 1 == 0) handler.Y = fieldSize - 2;
                     else handler.Y -= 1;
                     break;
 
                 case "DownArrow":
                 case "S":
-                    if (handler.Y + 1 == 9) handler.Y = 1;
+                    if (handler.Y + 1 == fieldSize - 1) handler.Y = 1;
                     else handler.Y += 1;
                     break;
             }
